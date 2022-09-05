8036842147608f948d4e9e0d6c44c63eabbca572
rupeshshinde22 <rupesh.shinde@springernature.com>
2022-09-05 12:50:22 +0530

    import render_template in routes we have create templates dir so that flask can fetch

--- app.py
+++ app.py
@@ -1,14 +1,20 @@
-from flask import Flask
+from flask import Flask,render_template
 
 app = Flask(__name__)
 
 
-# defined routes
+#1. defined routes(1st we used it)
+# @app.route('/')
+# def index():
+#     return "FLASK PROJECT"
+
+# 2.but now we are using render_template in routes
+# now we have create templates directory as same name because flask look for templates dir so that it can fetch
+#  render_template is used to generate output from a template file based
+#  on the Jinja2 engine that is found in the application's templates folder.
 @app.route('/')
 def index():
-    return "FLASK PROJECT"
-
-
+    return render_template('index.html')
 
 
 @app.route('/about')
@@ -16,7 +22,7 @@ def about():
     return "ABOUT FLASK PROJECT"
 
 
-#Second way to run app.py file is
+#1.Second way to run app.py file is
 if __name__ == '__main__':
     app.run(debug=True)
     #write in terminal as :::python app.py
